cc13a2f925cbc205b7ab4e84caa67c5a85b8925f
zvichaita <143297154+zvichaita@users.noreply.github.com>
2023-08-30 16:00:09 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,42 @@
-function getInspirationalMessage() {
-  var url = "https://quotes.rest/qod?category=inspire"; // API endpoint for inspirational quotes
+function countdown() {
+      // Set the target date for the countdown (August 27, 2028 00:00:00)
+      var targetDate = new Date("August 27, 2028 00:00:00").getTime();
 
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var quote = data.contents.quotes[0].quote;
-      var author = data.contents.quotes[0].author;
-      var message = quote + " - " + author;
-      console.log(message); // Output the inspirational message to the console
-    })
-    .catch(function(error) {
-      console.log("Unable to fetch an inspirational message at the moment.", error);
-    });
-}
+      // Update the countdown every second
+      var countdownInterval = setInterval(function() {
+        // Get the current date and time
+        var now = new Date().getTime();
 
-// Example usage
-getInspirationalMessage();
+        // Calculate the remaining time in milliseconds
+        var remainingTime = targetDate - now;
+
+        // Calculate the remaining time in days, hours, minutes, and seconds
+        var totalSeconds = Math.floor(remainingTime / 1000);
+        var days = Math.floor(totalSeconds / (24 * 60 * 60));
+        var hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+        var minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+        var seconds = totalSeconds % 60;
+
+        // Display the remaining time
+        var countdownDisplay =
+          days +
+          " days, " +
+          hours +
+          " hours, " +
+          minutes +
+          " minutes, " +
+          seconds +
+          " seconds of suffering left";
+
+        document.getElementById("countdown-timer").textContent = countdownDisplay;
+
+        // Stop the countdown when the target date is reached
+        if (remainingTime <= 0) {
+          clearInterval(countdownInterval);
+          document.getElementById("countdown-timer").textContent = "Countdown completed.";
+        }
+      }, 1000);
+    }
+
+    // Call the countdown function when the page has finished loading
+    window.onload = countdown;
